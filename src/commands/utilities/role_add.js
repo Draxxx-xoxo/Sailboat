96e2aa3fc23850e7c50bbf835412afd55050812e
jonesbang <35623215+jonesbang@users.noreply.github.com>
2021-08-31 15:58:12 +0800

    Error Messages

--- src/commands/utilities/role_add.js
+++ src/commands/utilities/role_add.js
@@ -6,6 +6,9 @@ module.exports = {
         const role = message.guild.roles.cache.get(args[0]);
         const user = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[1]));
 
+        if(!role) return message.channel.send('Please input a vaild role.')
+        if(!user) return message.channel.send('Please mention a user.')
+
 		for (let i = 0; i < user.roles.cache.map(r => r.id).length; i++) { 
         
             if(user.roles.cache.map(r => r.id)[i] == role){
