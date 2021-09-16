c62d1e49e5c165fe54932788f894189d871bbbf3
jonesbang <35623215+Draxxx-xoxo@users.noreply.github.com>
2021-09-16 21:43:37 +0800

    Update serverinfo.js

--- src/commands/utilities/serverinfo.js
+++ src/commands/utilities/serverinfo.js
@@ -5,22 +5,24 @@ module.exports = {
 	category: "botinfo",
 	description: "Returns bot and API latency in milliseconds.",
 	execute: async (message, args, client) => {
-        var user = message.mentions.users.first() || message.member.user
-        const member = message.guild.members.cache.get(user.id)
+
+        const guildid = args[0]
+
+        const guild = client.guilds.cache.get(guildid) || message.guild
     
         const serverinfo_embed = new MessageEmbed()
         .setAuthor(message.guild.name, message.guild.iconURL())
         .setThumbnail(message.guild.iconURL())
         .addFields(
-            { name: 'Owner', value: '<@!'+ message.guild.ownerID +'>', inline: true},
-            { name: 'Owner ID', value: message.guild.ownerID, inline: true},
-            { name: 'Server ID', value: message.guild.id, inline: true},
-            { name: 'Region', value: message.guild.region, inline: true },
-            { name: `Text Channels`, value: message.guild.channels.cache.size, inline: true },
+            { name: 'Owner', value: '<@!'+ guild.ownerID +'>', inline: true},
+            { name: 'Owner ID', value: guild.ownerID, inline: true},
+            { name: 'Server ID', value: guild.id, inline: true},
+            { name: 'Region', value: guild.region, inline: true },
+            { name: `Text Channels`, value: guild.channels.cache.size, inline: true },
             //{ name: `Voice Channels`, value: message.guild.channels.filter(c => c.type === 'voice').cache.size,inline: true },
-            { name: `Members`, value: message.guild.memberCount, inline: true },
-            { name: `Roles`, value: `${message.guild.roles.cache.size - 1}`, inline: true },
-            { name: `Server Creation`, value: new Date (message.guild.createdTimestamp).toLocaleString()},
+            { name: `Members`, value: guild.memberCount, inline: true },
+            { name: `Roles`, value: `${guild.roles.cache.size - 1}`, inline: true },
+            { name: `Server Creation`, value: new Date (guild.createdTimestamp).toLocaleString()},
         )
     
         message.channel.send(serverinfo_embed)
