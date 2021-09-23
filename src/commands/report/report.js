f43c4acf6c486856d38b7e4124adbb0363c759c1
jonesbang <35623215+Draxxx-xoxo@users.noreply.github.com>
2021-09-23 22:20:13 +0800

    Create report.js

---
+++ src/commands/report/report.js
@@ -0,0 +1,121 @@
+const {Client} = require('pg');
+const {pgkey} = require('../../../config.json');
+const {MessageEmbed} = require('discord.js');
+const Log = require('../../handlers/logging');
+const {MessageButton} = require("discord-buttons");
+const {command_logging, report_pugin, report_logging, report_logging_channel} = require('../../handlers/common_functions');
+
+
+module.exports = {
+	name: "report",
+	category: "botinfo",
+    permissions:[""],
+	description: "Returns bot and API latency in milliseconds.",
+	execute: async (message, args, discordclient) => {
+
+        if(await report_pugin(message.guild.id) == false){
+            return
+        }
+
+        if(message.mentions.roles.first()){
+            return message.channel.send('Why are you mentioning a role? <:blob_ping:807930234410237963>')
+        }
+
+        var member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+
+        if(!member){
+            message.channel.send('Please mention a user or input a user ID')
+            .then(msg => {
+                msg.delete({ timeout: 3000 })
+            });
+            return  
+        };
+
+
+        let reason_ = args.slice(1).join(" ").toString();
+
+        if (member.id == message.member.id){
+            message.channel.send('You cannot ban youself :person_facepalming:')
+            .then(msg => {
+                msg.delete({ timeout: 3000 })
+            });    
+            return
+        }
+
+        const client = new Client({
+            connectionString: pgkey,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
+    
+        await client.connect();
+
+
+        const query = `
+        INSERT INTO guild.reports(
+            reporter_id, reporter_tag, reported_user_tag, reported_user_id, reason, "timestamp", server_id, status)
+            VALUES (${message.author.id},'${message.author.tag}', '${member.user.tag}', ${member.user.id},'${reason_|| 'No Reason'}',${Date.now()}, ${message.guild.id}, 'pending')
+            RETURNING *
+        `
+        
+        const res = (await client.query(query)).rows[0]
+
+        message.channel.send(`User has been reported, please check your dm's for updates`)
+        .then(msg => {
+            msg.delete({ timeout: 3000 })
+          })
+        
+        //message.author.send('Overview about report').catch(() => message.reply("Can't send DM to your user!"));;
+
+        if(await report_logging(message.guild.id) ==  true){
+            Log.Send(
+                discordclient,
+                `${message.author.tag} reported ${member.user.tag}` + '`' + `${member.user.id}` + '`' + ` Reason: ${reason_ || 'None'}`,
+                message.guild.id
+            );
+            
+            const reportchannel = await report_logging_channel(message.guild.id);
+
+            const embed = new MessageEmbed()
+            .setTitle('Report #' + res.report_id)
+            .addFields(
+                {name: 'User', value: res.reported_user_tag + '\n`' + res.reported_user_id + '`', inline: true},
+                {name: 'Reporter', value: res.reporter_tag + '\n`' + res.reporter_id + '`', inline: true},
+                {name: 'Reason', value: res.reason}
+            );
+
+            let warn = new MessageButton()
+            .setLabel('Warn')
+            .setStyle('blurple')
+            .setID('warn');
+
+            let mute = new MessageButton()
+            .setLabel('Mute')
+            .setStyle('blurple')
+            .setID('mute');
+
+            let kick = new MessageButton()
+            .setLabel('Kick')
+            .setStyle('blurple')
+            .setID('Kick');
+
+            let ban = new MessageButton()
+            .setLabel('Ban')
+            .setStyle('blurple')
+            .setID('ban');
+
+            let deny = new MessageButton()
+            .setLabel('Deny')
+            .setStyle('blurple')
+            .setID('deny');
+            
+            message.guild.channels.cache.get(reportchannel).send({
+                buttons: [warn,mute,kick,ban,deny],
+                embed: embed
+                }); 
+        }
+        
+        await client.end();  
+	},
+};
