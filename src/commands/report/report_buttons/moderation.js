3527fa32c919725e03af8bcf33a90ef6a7897342
jonesbang <35623215+Draxxx-xoxo@users.noreply.github.com>
2021-10-04 18:27:38 +0800

    Update moderation.js

---
+++ src/commands/report/report_buttons/moderation.js
@@ -0,0 +1,175 @@
+const {reportlog} = require('../../../handlers/common_embeds');
+const functions = require('../../../handlers/common_functions');
+const Log = require('../../../handlers/logging');
+const {MessageEmbed} = require('discord.js');
+
+
+module.exports = {
+    deny: async (button, report_id, client, member, buttons) => {
+            const query = await functions.reportupdate(report_id, button, 'denied');
+
+            const res = (await client.query(query)).rows[0]
+
+            const report_log = await reportlog(res, '🔴')
+
+            button.reply.send(`${member.id} has been warned :ok_hand: User has been notified`);
+
+            button.message.edit({
+                embed: report_log,
+                buttons: buttons
+            })
+    },
+
+    warn: async (button, report_id, client, member, buttons, reason_, discordclient) => {
+        const moderator_id = button.message.author
+        const timestamp = Date.now();
+        const query = await functions.infractionQ(member, moderator_id, reason_, button, timestamp, 'warn')
+
+        await client.query(query);
+
+        const update = await functions.reportupdate(report_id, button, 'approved');
+        const updateres = (await client.query(update)).rows[0]
+
+        const report_log = await (await reportlog(updateres, '🟢')).addField('Infraction', 'Warn', false)
+
+        const embed = new MessageEmbed()
+        .setTitle(`You have been warned in ${button.guild.name}`)
+        .setDescription(`Reason\n` + '```' + reason_ + '```');
+
+        button.message.edit({
+            embed: report_log,
+            buttons: buttons
+        })
+
+        button.reply.send(`${member.id} has been warned :ok_hand: User has been notified`);
+
+
+        discordclient.users.cache.get(member.id).send(embed).catch(() => button.message.reply("Can't send DM to your user!"));
+
+        if(await functions.command_logging(button.guild.id) ==  true){
+            Log.Send(
+                discordclient,
+                `${moderator_id.username}#${moderator_id.discriminator} warned ${member.user.username}#${member.user.discriminator} ` + '`' + `${member.user.id}` + '`' + ` Reason: ${reason_ || 'None'}`,
+                message.guild.id
+            );
+        }
+    },
+
+    kick: async (button, report_id, client, member, buttons, reason_, discordclient) => {
+        const moderator_id = button.message.author
+        const timestamp = Date.now();
+        const query = await functions.infractionQ(member, moderator_id, reason_, button, timestamp, 'kick')
+
+        await client.query(query);
+
+        const update = await functions.reportupdate(report_id, button, 'approved');
+        const updateres = (await client.query(update)).rows[0]
+
+        const report_log = await (await reportlog(updateres, '🟢')).addField('Infraction', 'Kick', true)
+
+        member.kick()
+
+        const embed = new MessageEmbed()
+        .setTitle(`You have been kicked in ${button.guild.name}`)
+        .setDescription(`Reason\n` + '```' + reason_ + '```');
+
+        button.message.edit({
+            embed: report_log,
+            buttons: buttons
+        })
+
+        button.reply.send(`${member.id} has been kicked :ok_hand: User has been notified`);
+
+
+        discordclient.users.cache.get(member.id).send(embed).catch(() => button.message.reply("Can't send DM to your user!"));
+
+        if(await functions.command_logging(button.guild.id) ==  true){
+            Log.Send(
+                discordclient,
+                `${moderator_id.username}#${moderator_id.discriminator} kicked ${member.user.username}#${member.user.discriminator} ` + '`' + `${member.user.id}` + '`' + ` Reason: ${reason_ || 'None'}`,
+                message.guild.id
+            );
+        } 
+    },
+
+    ban: async (button, report_id, client, member, buttons, reason_, discordclient) => {
+        const moderator_id = button.message.author
+        const timestamp = Date.now();
+        const query = await functions.infractionQ(member, moderator_id, reason_, button, timestamp, 'ban')
+
+        await client.query(query);
+
+        const update = await functions.reportupdate(report_id, button, 'approved');
+        const updateres = (await client.query(update)).rows[0]
+
+        const report_log = await (await reportlog(updateres, '🟢')).addField('Infraction', 'Ban', true)
+
+        member.ban({reason: reason_})
+
+        const embed = new MessageEmbed()
+        .setTitle(`You have been banned in ${button.guild.name}`)
+        .setDescription(`Reason\n` + '```' + reason_ + '```');
+
+        button.message.edit({
+            embed: report_log,
+            buttons: buttons
+        })
+
+        button.reply.send(`${member.id} has been banned :ok_hand: User has been notified`);
+
+
+        discordclient.users.cache.get(member.id).send(embed).catch(() => button.message.reply("Can't send DM to your user!"));
+
+        if(await functions.command_logging(button.guild.id) ==  true){
+            Log.Send(
+                discordclient,
+                `${moderator_id.username}#${moderator_id.discriminator} kicked ${member.user.username}#${member.user.discriminator} ` + '`' + `${member.user.id}` + '`' + ` Reason: ${reason_ || 'None'}`,
+                message.guild.id
+            );
+        }
+    },
+
+    mute: async (button, report_id, client, member, buttons, reason_, discordclient) => {
+        const moderator_id = button.message.author
+        const timestamp = Date.now();
+        const query = await functions.infractionQ(member, moderator_id, reason_, button, timestamp, 'muted')
+
+        await client.query(query);
+
+        var role_id = await functions.muteRole(message);
+
+        if(!role_id){
+            return button.reply.send({content: 'Please add a mute role', ephemeral: true})
+        }
+
+        const update = await functions.reportupdate(report_id, button, 'approved');
+        const updateres = (await client.query(update)).rows[0]
+
+        const report_log = await (await reportlog(updateres, '🟢')).addField('Infraction', 'Mute', true)
+
+        member.role.add(role_id)
+
+        const embed = new MessageEmbed()
+        .setTitle(`You have been Muted in ${button.guild.name}`)
+        .setDescription(`Reason\n` + '```' + reason_ + '```');
+
+        button.message.edit({
+            embed: report_log,
+            buttons: buttons
+        })
+
+        button.reply.send(`${member.id} has been Muted :ok_hand: User has been notified`);
+
+
+        discordclient.users.cache.get(member.id).send(embed).catch(() => button.message.reply("Can't send DM to your user!"));
+
+        if(await functions.command_logging(button.guild.id) ==  true){
+            Log.Send(
+                discordclient,
+                `${moderator_id.username}#${moderator_id.discriminator} kicked ${member.user.username}#${member.user.discriminator} ` + '`' + `${member.user.id}` + '`' + ` Reason: ${reason_ || 'None'}`,
+                message.guild.id
+            );
+        }
+    }
+}
+
