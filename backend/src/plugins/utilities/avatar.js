88f15ef3b37db674f528b5488f9a01d0044f3f5b
jonesbang <35623215+Draxxx-xoxo@users.noreply.github.com>
2022-09-22 17:31:48 +0800

    eslint_add

--- backend/src/plugins/utilities/avatar.js
+++ backend/src/plugins/utilities/avatar.js
@@ -1,16 +1,16 @@
-const {MessageEmbed} = require('discord.js');
+const { MessageEmbed } = require("discord.js")
 
 module.exports = {
-	name: "avatar",
-	category: "botinfo",
-	description: "Returns bot and API latency in milliseconds.",
-	execute: async (message, args, client) => {
-        var user = message.mentions.users.first() || await message.guild.members.fetch(args[0]) ||message.member.user
+  name: "avatar",
+  category: "botinfo",
+  description: "Returns bot and API latency in milliseconds.",
+  execute: async (message, args, client) => {
+    const user = message.mentions.users.first() || await message.guild.members.fetch(args[0]) || message.member.user
 
-        const avatar_embed = new MessageEmbed()
-        .setTitle(`${user.user.username} Avatar`)
-        .setImage(user.displayAvatarURL());
+    const avatarEembed = new MessageEmbed()
+      .setTitle(`${user.user.username} Avatar`)
+      .setImage(user.displayAvatarURL())
 
-        message.channel.send({embeds: [avatar_embed]})
-	},
-};
+    message.channel.send({ embeds: [avatarEembed] })
+  }
+}
