ade17c2979de1073dd21a834b6251c7b6539568a
jonesbang <35623215+Draxxx-xoxo@users.noreply.github.com>
2022-10-17 22:35:22 +0800

    Report Slash Commands Update

--- backend/src/plugins/report/report.js
+++ backend/src/plugins/report/report.js
@@ -6,6 +6,7 @@ const {command_logging, report_pugin, report_logging, report_logging_channel} = 
 const {reportbuttons} = require("../../handlers/common_buttons")
 const deny = require("./report_buttons/moderation")
 const {reportlog} = require("../../handlers/common_embeds");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 
 module.exports = {
@@ -13,39 +14,25 @@ module.exports = {
   category: "",
   permissions:[""],
   description: "Returns bot and API latency in milliseconds.",
-  execute: async (message, args, discordclient) => {
+  execute: async (message, discordclient) => {
 
     if(await report_pugin(message.guild.id) == false){
+      message.reply('This server is not setup to accept reports. Please contact your administrator if you believe this is an error')
       return
     }
 
-    if(message.mentions.roles.first()){
-      return message.channel.send("Why are you mentioning a role? <:blob_ping:807930234410237963>")
-    }
-
-    var member = ""
-    if(message.mentions.members.first()){
-      member = message.mentions.members.first()
-    }  else if(args[0]){
-      member = await message.guild.members.fetch(args[0])
-    }
-        
-    if(!member){
-      message.channel.send("Please mention a user or input a user ID")
-        .then(msg => {
-          msg.delete({ timeout: 3000 })
-        });
-      return  
-    };
+    var member = await message.guild.members.fetch(message.options.getUser('user').id)
 
 
-    let reason_ = args.slice(1).join(" ").toString();
+    let reason_ = message.options.getString('reason')
 
     if (member.id == message.member.id){
-      message.channel.send("You cannot report youself :person_facepalming:")
-        .then(msg => {
-          msg.delete({ timeout: 3000 })
-        });    
+      message.reply({content: "You cannot report youself :person_facepalming:", fetchReply: true})
+
+      setTimeout(async function() {
+        await message.deleteReply()
+      }, 3000)
+
       return
     }
 
@@ -63,23 +50,24 @@ module.exports = {
     const query = `
         INSERT INTO public.reports(
             reporter_id, reporter_tag, reported_user_tag, reported_user_id, reason, guild_id, status)
-            VALUES (${message.author.id},'${message.author.tag}', '${member.user.tag}', ${member.user.id},'${reason_|| "No Reason"}', ${message.guild.id}, 'pending')
+            VALUES (${message.user.id},'${message.user.tag}', '${member.user.tag}', ${member.user.id},'${reason_|| "No Reason"}', ${message.guild.id}, 'pending')
             RETURNING *
         `
         
     const res = (await client.query(query)).rows[0]
 
-    message.channel.send("User has been reported, please check your dm's for updates")
-      .then(msg => {
-        msg.delete({ timeout: 3000 })
-      })
+    message.reply({content: "User has been reported, please check your DM's for updates", fetchReply: true})
+
+    setTimeout(async function() {
+      await message.deleteReply()
+    }, 3000)
         
     //message.author.send('Overview about report').catch(() => message.reply("Can't send DM to your user!"));;
 
     if(await report_logging(message.guild.id) ==  true){
       Log.Send(
         discordclient,
-        `${message.author.tag} reported ${member.user.tag}` + "`" + `${member.user.id}` + "`" + ` Reason: ${reason_ || "None"}`,
+        `${message.user.tag} reported ${member.user.tag}` + "`" + `${member.user.id}` + "`" + ` Reason: ${reason_ || "None"}`,
         message.guild.id
       );
             
@@ -88,14 +76,17 @@ module.exports = {
       const report_buttons = await reportbuttons(false)
 
       const embed = await reportlog(res,"🟡")
-
-            
+     
       message.guild.channels.cache.get(reportchannel).send({
         components: [report_buttons],
         embeds: [embed]
-      }); 
+      });  
     }
-        
     await client.end();  
   },
+  data: new SlashCommandBuilder()
+  .setName("report")
+  .setDescription("Report a user for moderators to review")
+  .addUserOption(option => option.setName("user").setDescription("Select a user").setRequired(true))
+  .addStringOption(option => option.setName("reason").setDescription("Reason for the report").setRequired(true))
 };
--- backend/src/plugins/report/report_search.js
+++ backend/src/plugins/report/report_search.js
@@ -6,6 +6,7 @@ const {command_logging, report_pugin, report_logging, report_logging_channel} = 
 const {reportbuttons} = require("../../handlers/common_buttons")
 const deny = require("./report_buttons/moderation")
 const {reportlog} = require("../../handlers/common_embeds");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 
 module.exports = {
@@ -13,22 +14,9 @@ module.exports = {
   category: "",
   permissions:[""],
   description: "Returns bot and API latency in milliseconds.",
-  execute: async (message, args, discordclient) => {
+  execute: async (message, discordclient) => {
 
-    var member = ""
-    if(message.mentions.members.first()){
-      member = message.mentions.members.first()
-    }  else if(args[0]){
-      member = await message.guild.members.fetch(args[0])
-    }
-
-    if(!member){
-      message.channel.send("Please mention a user or input a user ID")
-        .then(msg => {
-          msg.delete({ timeout: 3000 })
-        });
-      return  
-    };
+    var member = await message.guild.members.fetch(message.options.getUser('user').id)
 
     const client = new Client({
       user: process.env.user,
@@ -59,7 +47,7 @@ module.exports = {
 
 
     if(res.rowCount === 0){
-      message.channel.send("This user does not have any infractions for this server")
+      message.reply("This user does not have any infractions for this server")
       return
     }
 
@@ -71,18 +59,20 @@ module.exports = {
       var report = report_arrary.push(`[#${report_id_arrary[i]}]: ${reason_arrary[i] || "No Reason"}` + " `Status: " + status_arrary[i] + "`")
     }
 
-    var report_search = new MessageEmbed()
-      .setAuthor(`Infractions Overview for ${member.user.username}`, member.user.displayAvatarURL())
+    const report_search = new MessageEmbed()
+      .setAuthor({name: `Infractions Overview for ${member.user.username}`, iconURL: member.user.displayAvatarURL()})
       .setDescription("Click on the message link to jump to that report")
       .setThumbnail(member.user.displayAvatarURL())
       .addFields(
-        { name: "Infractions", value: report_arrary.slice(27).join("\n"), inline: false},
-        { name: `Joined ${message.member.guild.name}`, value: new Date (member.joinedTimestamp).toLocaleString(), inline: false},
+        { name: "Infractions", value: report_arrary.join("\n"), inline: false},
+        { name: `Joined ${message.guild.name}`, value: new Date (member.joinedTimestamp).toLocaleString(), inline: false},
       )
-    message.channel.send(report_search);
-
-    console.log();
+    message.reply({embeds: [report_search]});
 
     await client.end();
   },
+  data: new SlashCommandBuilder()
+  .setName("report_search")
+  .setDescription("Search for a user report.")
+  .addUserOption(option => option.setName("user").setDescription("Select a user").setRequired(true))
 };
