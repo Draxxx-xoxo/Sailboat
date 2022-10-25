ab83ebb67c74035001e774fbc29b163a6382e241
jonesbang <35623215+Draxxx-xoxo@users.noreply.github.com>
2022-10-25 09:27:14 +0800

    API add new configurator table

---
+++ API/src/api/configurator-v1/controllers/configurator-v1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * configurator-v1 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::configurator-v1.configurator-v1');
---
+++ API/src/api/configurator-v1/services/configurator-v1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * configurator-v1 service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::configurator-v1.configurator-v1');
