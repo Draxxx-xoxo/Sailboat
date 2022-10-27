b3111ac8613381db644831e288441a53d7b69ac2
jonesbang <35623215+Draxxx-xoxo@users.noreply.github.com>
2022-10-27 22:29:15 +0800

    Changing back to disabling role add and remove

--- backend/src/plugins/utilities/role_add.js
+++ backend/src/plugins/utilities/role_add.js
@@ -3,10 +3,11 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   name: "role_add",
   category: "botinfo",
+  enable: false,
   description: "Returns bot and API latency in milliseconds.",
   execute: async (message, args, client) => {
-    const role = await message.guild.roles.fetch(message.options.getRole('role').id);
-    const user = await message.guild.members.fetch(message.options.getUser('user').id);
+    const role = await message.guild.roles.fetch(message.options.getRole("role").id);
+    const user = await message.guild.members.fetch(message.options.getUser("user").id);
 
     for (let i = 0; i < user.roles.cache.map(r => r.id).length; i++) { 
         
@@ -21,8 +22,8 @@ module.exports = {
     }
   },
   data: new SlashCommandBuilder()
-  .setName("role_add")
-  .setDescription("Returns information about the role in the server")
-  .addRoleOption(option => option.setName("role").setDescription("The role to add to the user").setRequired(true))
-  .addUserOption(option => option.setName("user").setDescription("The user to have the role").setRequired(true))
+    .setName("role_add")
+    .setDescription("Returns information about the role in the server")
+    .addRoleOption(option => option.setName("role").setDescription("The role to add to the user").setRequired(true))
+    .addUserOption(option => option.setName("user").setDescription("The user to have the role").setRequired(true))
 };
--- backend/src/plugins/utilities/role_remove.js
+++ backend/src/plugins/utilities/role_remove.js
@@ -3,10 +3,11 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   name: "role_remove",
   category: "botinfo",
+  enable: false,
   description: "Returns bot and API latency in milliseconds.",
   execute: async (message, args, client) => {
-    const role = await message.guild.roles.fetch(message.options.getRole('role').id);
-    const user = await message.guild.members.fetch(message.options.getUser('user').id);
+    const role = await message.guild.roles.fetch(message.options.getRole("role").id);
+    const user = await message.guild.members.fetch(message.options.getUser("user").id);
 
     for (let i = 0; i < await user.roles.cache.map(r => r.id).length; i++) { 
         
@@ -20,8 +21,8 @@ module.exports = {
     }
   },
   data: new SlashCommandBuilder()
-  .setName("role_remove")
-  .setDescription("Returns information about the role in the server")
-  .addRoleOption(option => option.setName("role").setDescription("The role to add to the user").setRequired(true))
-  .addUserOption(option => option.setName("user").setDescription("The user to have the role").setRequired(true))
+    .setName("role_remove")
+    .setDescription("Returns information about the role in the server")
+    .addRoleOption(option => option.setName("role").setDescription("The role to add to the user").setRequired(true))
+    .addUserOption(option => option.setName("user").setDescription("The user to have the role").setRequired(true))
 };
